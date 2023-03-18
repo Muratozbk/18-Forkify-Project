a5087b094f282f1180135e5bcdb12ba86a7c4014
murat <slipstream11@yandex.com>
2023-03-18 14:29:42 +0300

    demo mvc

---
+++ demoContr.js
@@ -0,0 +1,21 @@
+import * as model from './demoModel.js'
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import recipeView from './src/js/views/recipeView.js';
+
+const recipeContainer = document.querySelector('.recipe');
+
+const controlRecipes = async function () {
+    try {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
+        recipeView.renderSpinner();
+        await model.loadRecipe(id);
+
+        recipeView.render(model.state.recipe)
+
+    } catch (err) { console.log(err) }
+};
+
+['hashchange', 'load'].forEach(el =>
+    window.addEventListener(el, controlRecipes))
