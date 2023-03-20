7820453083824c7762d8a483ac2e69f1c5be0691
murat <slipstream11@yandex.com>
2023-03-20 12:27:08 +0300

    render search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 // https://forkify-api.herokuapp.com/v2
 
 const controlRecipes = async function () {
@@ -26,13 +31,15 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+    console.log(recipeView)
     // 1)Get search query
     const query = searchView.getQuery();
     if (!query) return;
     // 2)Load Search Results
     await model.loadSearchResults(query);
     //3)Render Results
-    console.log(model.state.search.results)
+    resultsView.render(model.state.search.results);
   } catch (err) { console.log(err) }
 };
 
@@ -41,4 +48,5 @@ const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
 }
-init(); 
+init();
+
