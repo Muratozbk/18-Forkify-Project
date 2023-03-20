fb5f32eb84bba42a2e775c2362029cd58c0f5003
murat <slipstream11@yandex.com>
2023-03-20 17:25:15 +0300

    pagination 1

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,9 +6,9 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 // https://forkify-api.herokuapp.com/v2
 
 const controlRecipes = async function () {
@@ -39,7 +39,7 @@ const controlSearchResults = async function () {
     // 2)Load Search Results
     await model.loadSearchResults(query);
     //3)Render Results
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) { console.log(err) }
 };
 
